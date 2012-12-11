5b53d99370d34fcaeb1bad2b38c775092cfa90b5
SmallSlime <smallslime@gmail.com>
2012-12-11 14:15:36 +0800

    rebuild

---
+++ src/Slime/Framework/Http/response.php
@@ -0,0 +1,25 @@
+<?php
+namespace Slime\Framework\Http;
+
+class Response
+{
+    public function __construct()
+    {
+        ;
+    }
+
+    public function send()
+    {
+        ;
+    }
+
+    public function setHeader()
+    {
+        ;
+    }
+
+    public function setBody($body)
+    {
+        ;
+    }
+}
